e1283f223385b85f5ab505df5e999a4a50294470
Nithesh-TeachMint <nitesh.external@teachmint.com>
2022-06-13 10:38:21 +0530

    firsFincareCommit

---
+++ src/test/java/com/fincare/tests/demo.java
@@ -0,0 +1,11 @@
+package com.fincare.tests;
+
+public class demo
+{
+    public static void main (String []args)
+
+    {
+        System.out.println("raghu");
+        System.out.println("12345");
+    }
+}
